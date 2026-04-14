92a65f3f80ec36cf46ea64e0a5bf109b7791de67
Sascha <sascha-henkel@gmx.net>
2026-04-14 16:44:31 +0200

    make an actual component

--- src/app/features/dnd/spells/spell-editor/spell-editor.component.ts
+++ src/app/features/dnd/spells/spell-editor/spell-editor.component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-spell-editor',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './spell-editor.component.html',
-  styleUrl: './spell-editor.component.scss',
+  styleUrls: ['./spell-editor.component.scss'],
 })
-export class SpellEditorComponent {}
+export class SpellEditorComponent {
+
+  public levels: number[] = [0,1,2,3,4,5,6,7,8,9];
+
+  public schoolOptions = [
+    { value: 'abjuration', label: 'Abjuration' },
+    { value: 'conjuration', label: 'Conjuration' },
+    { value: 'divination', label: 'Divination' },
+    { value: 'enchantment', label: 'Enchantment' },
+    { value: 'evocation', label: 'Evocation' },
+    { value: 'illusion', label: 'Illusion' },
+    { value: 'necromancy', label: 'Necromancy' },
+    { value: 'transmutation', label: 'Transmutation' },
+  ];
+
+  public spell = new FormGroup({
+    name: new FormControl<string>(''),
+    description: new FormControl<string>(''),
+    level: new FormControl<number>(0),
+    school: new FormControl<string>('abjuration'),
+  });
+
+  public setLevel(level: number) {
+    this.spell.controls.level.setValue(level);
+  }
+
+  public submit() {
+    console.log(this.spell.value);
+  }
+}
