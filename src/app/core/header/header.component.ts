2a6d9de3e6af95a3f0e80c6635bf52c8399b90c8
Sascha <sascha-henkel@gmx.net>
2026-06-25 14:55:54 +0200

    some more CSS fixes

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../services/theme.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  imports: [RouterLink],
 })
 export class HeaderComponent {
   menuOpen = false;
--- src/app/features/art/art.component.ts
+++ src/app/features/art/art.component.ts
@@ -10,42 +10,53 @@ import { Artwork } from '../../core/models/artwork';
 
 export class ArtComponent {
 
-  public artworks: Artwork[] = [
-    {
-      id: 1,
-      title: "Apple",
-      description: "",
-      imageUrl: "/art/my_first_apple.png",
-      createdAt: new Date('2026-06-25'),
-      updatedAt: new Date('2026-06-25'),
-      author: "BafflingBonk"
-    },
-    {
-      id: 2,
-      title: "Jug",
-      description: "",
-      imageUrl: "/art/first_jug.png",
-      createdAt: new Date('2026-06-25'),
-      updatedAt: new Date('2026-06-25'),
-      author: "BafflingBonk"
-    },
-    {
-      id: 3,
-      title: "Dark Castle Background",
-      description: "",
-      imageUrl: "/art/dark_castle_background.png",
-      createdAt: new Date('2026-06-25'),
-      updatedAt: new Date('2026-06-25'),
-      author: "BafflingBonk, WafflingWonk"
-    },
-    {
-      id: 3,
-      title: "Mansion",
-      description: "",
-      imageUrl: "/art/mansion.png",
-      createdAt: new Date('2026-06-25'),
-      updatedAt: new Date('2026-06-25'),
-      author: "BafflingBonk, WafflingWonk"
-    }
-  ];
+  public selectedArtwork: any | null = null;
+  public artworks: Artwork[] = artworks;
+
+  openArtwork(artwork: any) {
+    this.selectedArtwork = artwork;
+  }
+
+  closeArtwork() {
+    this.selectedArtwork = null;
+  }
 }
+
+const artworks: Artwork[] = [
+  {
+    id: 1,
+    title: "Apple",
+    description: "",
+    imageUrl: "/art/my_first_apple.png",
+    createdAt: new Date('2026-06-25'),
+    updatedAt: new Date('2026-06-25'),
+    author: "BafflingBonk"
+  },
+  {
+    id: 2,
+    title: "Jug",
+    description: "",
+    imageUrl: "/art/first_jug.png",
+    createdAt: new Date('2026-06-25'),
+    updatedAt: new Date('2026-06-25'),
+    author: "BafflingBonk"
+  },
+  {
+    id: 3,
+    title: "Dark Castle Background",
+    description: "",
+    imageUrl: "/art/dark_castle_background.png",
+    createdAt: new Date('2026-06-25'),
+    updatedAt: new Date('2026-06-25'),
+    author: "BafflingBonk, WafflingWonk"
+  },
+  {
+    id: 3,
+    title: "Mansion",
+    description: "",
+    imageUrl: "/art/mansion.png",
+    createdAt: new Date('2026-06-25'),
+    updatedAt: new Date('2026-06-25'),
+    author: "BafflingBonk, WafflingWonk"
+  }
+];
